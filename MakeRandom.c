1971cb32718ad2ca39d1a81391812e3bbb73cd91
Z00Honey <80230097+Z00Honey@users.noreply.github.com>
2021-07-06 00:51:23 +0900

    양/음 랜덤값 만들기 Test

--- MakeRandom.c
+++ MakeRandom.c
@@ -6,6 +6,11 @@ int main()
 	unsigned char pt[16] = { 0x00, 0x11,0x22,0x33 ,0x44 ,0x55 ,0x66,0x77,0x88,0x99,0xaa,0xbb,0xcc,0xdd,0xee,0xff };
 	char *p = pt; // 포인터의 자료만 바꾸기 > 값을 참조할 때 unsinged를 무시하고 참조
 
+	char pt_[16];
+	for (int i = 0; i<16; i++)
+	{
+		pt_[i] = pt[i];
+	}
 
 	printf("<< unsigned char -> int 출력 >> \n");
 	for (int i = 0; i < 16; i++)
@@ -20,10 +25,15 @@ int main()
 	{
 		printf("%d ", p[i]);
 	}
-	printf("\n");
+	printf("\n\n");
 
 
-
+	printf("<< char -> int 출력 >> \n");
+	for (int i = 0; i < 16; i++)
+	{
+		printf("%d ", pt_[i] / 50);
+	}
+	printf("\n\n");
 
 	return 0;
 }
