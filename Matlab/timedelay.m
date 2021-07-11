1087530a27fd670f12c62a61b7fc9ffc2a1bc387
Z00Honey <80230097+Z00Honey@users.noreply.github.com>
2021-07-11 14:50:11 +0900

    Update timedelay.m

--- Matlab/timedelay.m
+++ Matlab/timedelay.m
@@ -1,17 +1,17 @@
 function corr = timedelay(standard , trace, trNum)
 
-startindex = 27; % 1 | 27
-startpoint = 1; % 1 | 1271000
+startindex = 1; % 1 | 27
+startpoint = 1; % [ 1 ~ ( length(trace(:,i)) - length(standard) )]  1271000
 
 % 기준파형을 밀면서 상관계수 구하기
 for i = startindex : ( startindex + trNum )
     
-    range = length(trace(:,i)); % length(trace(:,i))
+    range = (length(trace(:,i)) - length(standard)); % [ 1 ~ ( length(trace(:,i)) - length(standard) ) ]
     
-    for j = startpoint : ( startpoint + range )
+    for j = startpoint : ( startpoint + range - 1 )
         
         % correalation coefficient
-        temp = corrcoef(standard , trace(j:length(standard)+j - 1 ,i));
+        temp = corrcoef(standard , trace(j:length(standard) + j - 1 ,i));
         corr(j,i) = temp(1,2);
         
         % cross correlation
